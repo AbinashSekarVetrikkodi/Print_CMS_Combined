6cca5edac4d7cc9c3dc2a26e2a152e9d6d054086
AbinashSekarVetrikkodi <142879144+AbinashSekarVetrikkodi@users.noreply.github.com>
2024-07-05 18:45:07 +0530

    removed download
    removed download

--- client/src/Components/Thumbnail.jsx
+++ client/src/Components/Thumbnail.jsx
@@ -46,8 +46,6 @@ const Thumbnail = () => {
     }
   };
 
-
-
   const initialIssueDate =
     sessionStorage.getItem("issueDate") || getCurrentDate();
   const [issueDate, setIssueDate] = useState(initialIssueDate);
@@ -396,44 +394,10 @@ const Thumbnail = () => {
     }
   };
  
-  // const fetchPDFs = async (imageUrls) => {
-  //   if (!Array.isArray(imageUrls) || imageUrls.length === 0) return;
- 
-  //   const urls = await Promise.all(
-  //     imageUrls.map(async (imageUrl) => {
-  //       const url = new URL(imageUrl);
-  //       const params = new URLSearchParams(url.search);
-  //       const formattedDate = params.get("date");
-  //       const zone = params.get("zone");
-  //       const product = params.get("product");
-  //       const page = params.get("page");
-  //       const edition = params.get("edition");
- 
-  //       try {
-  //         const response = await fetch(
-  //           `${process.env.REACT_APP_IPCONFIG}api/reporter/pdf?date=${formattedDate}&zone=${zone}&product=${product}&page=${page}&edition=${edition}`
-  //         );
-  //         if (response.ok) {
-  //           const pdfUrl = response.url;
-            
-  //           window.open(pdfUrl, "_blank"); // Open the PDF in a new tab
-  //         } else {
-  //           console.error(
-  //             `Failed to fetch PDF for page ${page}. Status: ${response.status}, ${response.statusText}`
-  //           );
-  //         }
-  //       } catch (error) {
-  //         console.error(`Error fetching PDF for page ${page}:`, error);
-  //       }
-  //     })
-  //   );
-  // };
- 
-
   const fetchPDFs = async (imageUrls) => {
     if (!Array.isArray(imageUrls) || imageUrls.length === 0) return;
-  
-    await Promise.all(
+ 
+    const urls = await Promise.all(
       imageUrls.map(async (imageUrl) => {
         const url = new URL(imageUrl);
         const params = new URLSearchParams(url.search);
@@ -442,23 +406,15 @@ const Thumbnail = () => {
         const product = params.get("product");
         const page = params.get("page");
         const edition = params.get("edition");
-  
+ 
         try {
           const response = await fetch(
             `${process.env.REACT_APP_IPCONFIG}api/reporter/pdf?date=${formattedDate}&zone=${zone}&product=${product}&page=${page}&edition=${edition}`
           );
-  
           if (response.ok) {
-            const blob = await response.blob();
-            const filename = `${product}_${formattedDate}_${zone}_${edition}_${page}.pdf`;
-  
-            const link = document.createElement('a');
-            link.href = URL.createObjectURL(blob);
-            link.download = filename;
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-            URL.revokeObjectURL(link.href);
+            const pdfUrl = response.url;
+            
+            window.open(pdfUrl, "_blank"); // Open the PDF in a new tab
           } else {
             console.error(
               `Failed to fetch PDF for page ${page}. Status: ${response.status}, ${response.statusText}`
@@ -470,6 +426,48 @@ const Thumbnail = () => {
       })
     );
   };
+ 
+
+  // const fetchPDFs = async (imageUrls) => {
+  //   if (!Array.isArray(imageUrls) || imageUrls.length === 0) return;
+  
+  //   await Promise.all(
+  //     imageUrls.map(async (imageUrl) => {
+  //       const url = new URL(imageUrl);
+  //       const params = new URLSearchParams(url.search);
+  //       const formattedDate = params.get("date");
+  //       const zone = params.get("zone");
+  //       const product = params.get("product");
+  //       const page = params.get("page");
+  //       const edition = params.get("edition");
+  
+  //       try {
+  //         const response = await fetch(
+  //           `${process.env.REACT_APP_IPCONFIG}api/reporter/pdf?date=${formattedDate}&zone=${zone}&product=${product}&page=${page}&edition=${edition}`
+  //         );
+  
+  //         if (response.ok) {
+  //           const blob = await response.blob();
+  //           const filename = `${product}_${formattedDate}_${zone}_${edition}_${page}.pdf`;
+  
+  //           const link = document.createElement('a');
+  //           link.href = URL.createObjectURL(blob);
+  //           link.download = filename;
+  //           document.body.appendChild(link);
+  //           link.click();
+  //           document.body.removeChild(link);
+  //           URL.revokeObjectURL(link.href);
+  //         } else {
+  //           console.error(
+  //             `Failed to fetch PDF for page ${page}. Status: ${response.status}, ${response.statusText}`
+  //           );
+  //         }
+  //       } catch (error) {
+  //         console.error(`Error fetching PDF for page ${page}:`, error);
+  //       }
+  //     })
+  //   );
+  // };
   
 
   const handleThumbnailClick = (imageUrl) => {
